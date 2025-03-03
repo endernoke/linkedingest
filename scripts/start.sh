ec71b625e232625f8140ca25d01356dae271b496
James Zheng <endernoke@gmail.com>
2025-03-03 22:41:44 +0800

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,8 +3,8 @@ HOST="0.0.0.0"
 PORT="${PORT:-10000}"
 if [ -z "$PORT" ]; then
     echo "Starting FastAPI app on $HOST:10000"
-    python -m uvicorn app.main:app --host $HOST --port 10000
+    uvicorn app.main:app --host $HOST --port 10000
 else
     echo "Starting FastAPI app on $HOST:$PORT"
-    python -m uvicorn app.main:app --host $HOST --port $PORT
+    uvicorn app.main:app --host $HOST --port $PORT
 fi
